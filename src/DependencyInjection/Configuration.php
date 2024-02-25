ff471b5d633da2b14a419357a8485fe24e11319d
luigi massa <lmassa@bwlab.it>
2024-02-25 20:39:55 +0100

    upgrade to symfony 7

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mb\DoctrineLogBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * @package Mb\DoctrineLogBundle\DependencyInjection
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+	    $treeBuilder = new TreeBuilder('mb_doctrine_log');
+	    if (method_exists($treeBuilder, 'getRootNode')) {
+		    $rootNode = $treeBuilder->getRootNode();
+	    } else {
+		    // for symfony/config 4.1 and older
+		    $rootNode = $treeBuilder->root('mb_doctrine_log');
+	    }
+
+        $rootNode
+            ->children()
+                ->arrayNode('ignore_properties')->prototype('scalar')->end()
+            ->end()
+            ->scalarNode('entity_manager')
+                ->defaultValue('default')
+            ->end()
+            ->scalarNode('listener_class')
+                ->defaultValue('Mb\DoctrineLogBundle\EventListener\Logger')
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
